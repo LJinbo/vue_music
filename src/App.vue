3883796d1a234716cdf6f2e49a145882df15ddc5
wdl <wdl@heshuruanjian.com>
2018-09-03 18:07:53 +0800

    aa

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <headers></headers>
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import Headers from "./components/Hader/Hader";
+  export default {
+    name: 'App',
+    components: {
+      Headers,
+    }
+  }
+</script>
+
+<style lang="scss">
+</style>
---
+++ src/components/Hader/Hader.vue
@@ -0,0 +1,17 @@
+<template>
+<header>
+<div class="logo"></div>
+</header>
+</template>
+<script>
+export default{
+
+}
+</script>
+<style lang="scss">
+@import "common/scss/_variable.scss";
+header{
+background:$theme_bg;
+height: 100px;
+}
+</style>
